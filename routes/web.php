74077688d4851df25d50ffd03e1347c4b87487c2
Miretazam Ciptaprima <mcrealdeviant@gmail.com>
2024-04-27 14:36:03 +0700

    Restrict route to homepage

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\UserController;
 
-Route::view('/', 'homepage');
 
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
 Route::middleware('auth')->group(function () {
+    Route::view('/', 'homepage');
+    
     Route::get('/users', [UserController::class, 'index'])->name('user.list');
 });
